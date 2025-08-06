46d954cf010b5f56d7489adaf3a304fae29e03ae
Joan <joanm95@gmail.com>
2025-08-06 14:22:43 -0400

    Initial Commit

---
+++ InteractiveUI4.5/InteractiveUI4_5App.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveUI4_5App.swift
+//  InteractiveUI4.5
+//
+//  Created by Scholar on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveUI4_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
